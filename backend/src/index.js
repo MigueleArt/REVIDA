e62905d66f3562e245ec6c714f9fa224b48d413d
a3523110714@alumno.uttehuacan.edu.mx <a3523110714@alumno.uttehuacan.edu.mx>
2026-02-02 00:25:29 -0600

    feat: manejo de errores 404 en el servidor BE

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,7 +1,24 @@
 import http from "http";
 
 const server = http.createServer((req, res) => {
-  res.end("Revida backend running");
+  res.setHeader("Content-Type", "application/json");
+
+  if (req.url === "/") {
+    res.writeHead(200);
+    res.end(JSON.stringify({ 
+        status: "success", 
+        message: "Revida backend running" 
+    }));
+  } 
+  // error 404
+  else {
+    res.writeHead(404);
+    res.end(JSON.stringify({ 
+      error: 404, 
+      message: "La ruta solicitada no existe en el sistema REVIDA",
+      hint: "Verifica si escribiste bien la URL"
+    }));
+  }
 });
 
 server.listen(3000, () => {
