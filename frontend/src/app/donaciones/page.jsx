e56166f72e741d3bd8569266f914ebede41b5236
Jose Miguel Jimenez <jmigueljimenez910@gmail.com>
2026-02-20 09:35:55 -0600

    fix/PaginaDonacionesArreglo

---
+++ frontend/src/app/donaciones/page.jsx
@@ -0,0 +1,9 @@
+export default function DonacionesPage() {
+    return (
+        <div style={{ padding: '2rem' }}>
+            <h1>Donaciones</h1>
+            <p>Listado de donaciones disponibles.</p>
+        </div>
+    );
+
+}
